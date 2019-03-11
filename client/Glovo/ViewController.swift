f505f7df2b882817e40e09f326583fd2d0b3b425
Eugene Peschard <e@peschard.me>
2019-03-11 12:41:26 +0100

    initial commit new project using  Single View App template

---
+++ client/Glovo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Glovo
+//
+//  Created by Eugène Peschard on 11/03/2019.
+//  Copyright © 2019 pesch.app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
